37318c3f37c52ef77e79bddfa49ea3a3052848a3
Chris Mullins <cmullins@liveramp.com>
2017-01-21 13:36:01 -0800

    use maven central ASK lib

--- bin/run.sh
+++ bin/run.sh
@@ -8,5 +8,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 
-mvn install:install-file -Dfile=lib/alexa-skills-kit-1.0.120.0.jar -DgroupId=amazon -DartifactId=amazon-alexa -Dversion=1.0.120.0 -Dpackaging=jar
 mvn clean compile exec:java -Dexec.mainClass="org.sidoh.alexa_dispatcher.AlexaDispatcherServer" -Dexec.args="$DIR/../config/config.yml"
